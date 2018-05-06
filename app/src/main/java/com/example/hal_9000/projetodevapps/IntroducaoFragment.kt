1e8960f616751c14f571bfeade9cae451c910c7d
Alon Mota <alon.mota.l@gmail.com>
2018-05-06 15:06:47 -0300

    pequenas correções

--- app/src/main/java/com/example/hal_9000/projetodevapps/IntroducaoFragment.kt
+++ app/src/main/java/com/example/hal_9000/projetodevapps/IntroducaoFragment.kt
@@ -1,12 +1,14 @@
 package com.example.hal_9000.projetodevapps
 
 import android.app.Fragment
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.introducao_fragment.*
 
 /**
  * Created by Danilo José Bispo Galvão on 04/05/2018.
@@ -16,6 +18,19 @@ class IntroducaoFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val rootView = inflater!!.inflate(R.layout.introducao_fragment, container, false)
 
+        button_int_adotar.setOnClickListener {
+
+        }
+
+        button_int_ajudar.setOnClickListener {
+            // Handler code here.
+
+        }
+
+        button_int_cadastrar.setOnClickListener {
+            // Handler code here.
+
+        }
         return rootView
     }
 }
