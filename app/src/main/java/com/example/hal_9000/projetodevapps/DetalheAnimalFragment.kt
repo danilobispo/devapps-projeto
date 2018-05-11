ab8d215fb543cfb67b19322abe17fa20567aff82
Alon Mota <alon.mota.l@gmail.com>
2018-05-10 22:41:41 -0300

    pupulando tela

--- app/src/main/java/com/example/hal_9000/projetodevapps/DetalheAnimalFragment.kt
+++ app/src/main/java/com/example/hal_9000/projetodevapps/DetalheAnimalFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.example.hal_9000.projetodevapps.Model.Animal
 import java.io.Serializable
+import java.nio.file.Files.find
 
 class DetalheAnimalFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -15,8 +16,13 @@ class DetalheAnimalFragment : Fragment() {
         val args = arguments;
         val animal:Animal = args.getSerializable("animal") as Animal
 
-        //TODO popular as views com os valores contidos em animal.
-        //ex: R.id.detalhe_animal_nome . text = animal.nome;
+        rootView?.findViewById<android.support.v7.widget.AppCompatTextView>(R.id.detalhe_animal_nome)!!.text = animal.name
+        rootView?.findViewById<android.support.v7.widget.AppCompatTextView>(R.id.detalhe_animal_sexo)!!.text = animal.sexo
+        rootView?.findViewById<android.support.v7.widget.AppCompatTextView>(R.id.detalhe_animal_nome2)!!.text = animal.name
+        rootView?.findViewById<android.support.v7.widget.AppCompatTextView>(R.id.detalhe_animal_local)!!.text = animal.localizacao
+        rootView?.findViewById<android.support.v7.widget.AppCompatTextView>(R.id.detalhe_animal_porte)!!.text = animal.porte
+        rootView?.findViewById<android.support.v7.widget.AppCompatTextView>(R.id.detalhe_animal_idade)!!.text = animal.idade.toString()
+
         return rootView
     }
 }
