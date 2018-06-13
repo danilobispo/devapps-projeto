5504de66c9901e595798e260c8f3188127c7d013
DaniloBispo <danilobispogalvao@gmail.com>
2018-06-13 15:33:52 -0300

    Inserção do FirebaseFirestore nas dependências do gradle e nos arquivos de configuração do firebase

--- app/src/main/java/com/example/hal_9000/projetodevapps/ConfiguracaoFirebase.java
+++ app/src/main/java/com/example/hal_9000/projetodevapps/ConfiguracaoFirebase.java
@@ -1,31 +1,36 @@
 package com.example.hal_9000.projetodevapps;
 
-import android.support.annotation.NonNull;
-
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ConfiguracaoFirebase {
 
     private static DatabaseReference refenciaFirebase;
     private static FirebaseAuth autenticacao;
+    private static FirebaseFirestore firestoreReferencia;
 
-    public static DatabaseReference getFirebase(){
-        if(refenciaFirebase == null){
+    public static DatabaseReference getFirebase() {
+        if (refenciaFirebase == null) {
             refenciaFirebase = FirebaseDatabase.getInstance().getReference();
         }
         return refenciaFirebase;
 
     }
 
-    public static FirebaseAuth getFirebaseAutenticacao(){
-        if(autenticacao == null){
+    public static FirebaseAuth getFirebaseAutenticacao() {
+        if (autenticacao == null) {
             autenticacao = FirebaseAuth.getInstance();
         }
         return autenticacao;
+    }
 
+    public static FirebaseFirestore getFirestoreReferencia() {
+        if(firestoreReferencia == null) {
+            firestoreReferencia = FirebaseFirestore.getInstance();
+        }
+        return firestoreReferencia;
     }
 
 
